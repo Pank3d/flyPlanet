4aab25e1643335c8c1920455aa425a246ef97885
m.jilevskiy <m.jilevskiy@aerodisk.ru>
2026-01-09 15:55:31 +0300

    fix: fix shortId

--- bot/src/configStatic/config.ts
+++ bot/src/configStatic/config.ts
@@ -1,38 +1,43 @@
 import { ProfilesType } from "./types.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SHORT_ID = process.env.SHORT_ID || "";
 
 export const operatorProfiles: ProfilesType[] = [
   {
     port: 443,
     sni: "yandex.ru",
-    shortId: "a1b2c3d4",
+    shortId: SHORT_ID,
     name: "Yandex",
     comment: "Универсальный, работает с Tele2, Билайн",
   },
   {
     port: 8443,
     sni: "vk.com",
-    shortId: "e5f6g7h8",
+    shortId: SHORT_ID,
     name: "VK",
     comment: "Хорошо для МТС, Билайн",
   },
   {
     port: 2053,
     sni: "mail.ru",
-    shortId: "i9j0k1l2",
+    shortId: SHORT_ID,
     name: "Mail.ru",
     comment: "Альтернатива для Билайн, Tele2",
   },
   {
     port: 2083,
     sni: "ok.ru",
-    shortId: "m3n4o5p6",
+    shortId: SHORT_ID,
     name: "OK.ru",
     comment: "Для Мегафон, МТС",
   },
   {
     port: 2087,
     sni: "rutube.ru",
-    shortId: "q7r8s9t0",
+    shortId: SHORT_ID,
     name: "RuTube",
     comment: "Альтернативный порт для всех операторов",
   },
